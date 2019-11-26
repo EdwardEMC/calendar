003cad3e62ccb7a62f468905d8e11946c8cd2f13
EMC <edwardcoad.ec@gmail.com>
2019-11-26 16:02:44 +1030

    Html skeleton done, working on dynamic jquery list entries

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function listDisplay(){
+    for(i=0; i<9; i++) {
+        var row = $("<div>");
+        var column = $("<div>");
+        var input = $("<div>");
+        
+        row.attr("class", "row");
+    }
+}
